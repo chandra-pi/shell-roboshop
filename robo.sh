037369c5afec0599c46eb8500700fdd7e41c3e54
chandra-pi <chandrahyd.45@gmail.com>
2026-07-14 21:34:57 +0530

    hello robo

--- robo.sh
+++ robo.sh
@@ -9,16 +9,12 @@ DOMAIN_NAME="devaws84s.online"
 
 for instance in ${INSTANCES[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t2.micro 
-    --security-group-ids sg-0beda789759f4ad50 --tag-specifications "ResourceType=instance,
-    Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t2.micro --security-group-ids sg-0beda789759f4ad50 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend"]
     then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID 
-        --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID 
-        --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     fi
     echo "$instance equal to : $IP"
 done
