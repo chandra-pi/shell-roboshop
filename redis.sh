175fedb31887968bb6097f5423b7c0e0ec5a76fb
chandra-pi <chandrahyd.45@gmail.com>
2026-07-15 16:12:02 +0530

    hello robo

--- redis.sh
+++ redis.sh
@@ -46,10 +46,10 @@ VALIDATE $? "Installing Redis"
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Edited redis.conf to accept remote connections"
 
-systemctl enable redis
+systemctl enable redis &>>LOG_FILE
 VALIDATE $? "Enable redis"
 
-systemctl start redis
+systemctl start redis &>>LOG_FILE
 VALIDATE $? "Started redis"
 
 END_TIME=$(date +%s)
