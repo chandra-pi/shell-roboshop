2044ec55eb9c5b5680c98ffbaee74535231a9de6
chandra-pi <chandrahyd.45@gmail.com>
2026-07-15 15:42:50 +0530

    hello robo

---
+++ redis.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/roboshop-log"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+#Check the user has root previliges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR: Please run the script with root access $N" | tee -a $LOG_FILE
+    exit 1
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+#Validate function takes input as exit status, what command they try to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is... $G Success $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is.... $R Failure $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable redis -y
+VALIDATE $? "Disabling Default Redis version"
+
+dnf module enable redis:7 -y
+VALIDATE $? "Enabling Redis:7"
+
+dnf install redis -y
+VALIDATE $? "Installing Redis"
+
+
+
+
